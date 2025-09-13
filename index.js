138106d8c866666d5d694011a589d0c40f1e40ef
Renehilwe M <renehilwec07@gmail.com>
2025-09-13 13:49:41 +0200

    api intergration is added to the project

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
+function displayPoem(response) {
+    console.log("poem generated")
+  new Typewriter('#poem', {
+    strings: response.data.answer,
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+});
+
+}
+
 function generatePoem(event){
     event.preventDefault();
 
-let  poemElement= document.querySelector("#poem");
+let instructionsInput = document.querySelector("#user-instructions");
+let apiKey = "b99t41fb7bao38b2a301ca5200fbfdb3";
+let prompt = `Generate concise but meaninful poem about ${instructionsInput.value}`;
+let context = " your an outstanding poem expert and known for short poem. your mission is to generate a 5 line poem following the user-instructions. each line must have <br />";
+let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-new Typewriter('#poem', {
-  strings: "Love never fades",
-  autoStart: true,
-  delay: 1,
-  cursor: "",
-});
+console.log("Generating poem")
+console.log(`Prompt: ${prompt}`);
+console.log(`Context: ${context}`)
+
+axios.get(apiURL).then(displayPoem);
 
 }
 
